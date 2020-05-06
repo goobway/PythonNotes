2d88c500fa2f5e205421caedfd51f78ba9321090
goobway <calistagreenway@gmail.com>
2020-05-05 20:16:07 -0400

    more i/o

--- reading_writing_files.py
+++ reading_writing_files.py
@@ -17,15 +17,48 @@ Why do we need inputs?
  - managing and returning the output is the postprocessing stage
 '''
 
-# funtions input and print 
+# funtions input and print
 # print can be used to display info on screen
-# input can be used to display some info and prompt the user to enter a string 
+# input can be used to display some info and prompt the user to enter a string
 print("Quadratic Equation f(x)=ax**2+bx+c")
 
-## ask user to input coefficient values
-a=input("Enter value for a: ")
-b=input("Enter value for b: ")
-c=input("Enter value for c: ")
+# ask user to input coefficient values
+a = input("Enter value for a: ")
+b = input("Enter value for b: ")
+c = input("Enter value for c: ")
 
-## display user equation
-print("Equation is: (%s)x**2(%s)x+(%s)"%(a,b,c))
+# display user equation
+print("Equation is: (%s)x**2(%s)x+(%s)" % (a, b, c))
+
+
+# I/O REVISITED
+'''
+Input can be obtained in many different ways:
+ - from the keyboard
+ - from command-line argument
+ - from a GUI
+Output can be provided:
+ - as text on a screen
+ - by writing in a file
+ - as graphics that will be displayed in a window
+Python offers some useful objects and methods that ease the way we can handle I/O
+'''
+
+# INPUT FROM THE KEYBOARD
+'''
+First let us learn about the split() method
+ - it is a method from the class str (acts on String objects)
+ - it is used to separate (split) a given string into a list of strings
+ - it can identifiy any delimiters/separators
+'''
+phrase = "Welcome to ECE 122"
+mylist = phrase.split()  # split() uses blank separator as default
+print(mylist)
+
+phrase = "Welcome, to, ECE, 122"
+mylist = phrase.split(", ")
+print(mylist)
+
+# second, let us learn about a trick for splitting list
+i1, i2, i3, i4 = ["Welcome", "to", "New", "York"]
+print(i1,i2,i3,i4)
