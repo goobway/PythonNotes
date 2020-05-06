d795ba1ae6d5f4bd33cc16f3f0f13e39151d130c
goobway <calistagreenway@gmail.com>
2020-05-05 20:03:43 -0400

    i/o comments

--- reading_writing_files.py
+++ reading_writing_files.py
@@ -3,5 +3,29 @@
 
 '''
 I/O Review
+Why do we need inputs?
+ - obtain information from the user 
+ - initalize (set) some data
 
+ Why do we need outputs?
+ - return information to the user
+ - get computed data values
+
+ In programming:
+ - gathering the input is the preprocessing stage
+ - computing is the processing stage
+ - managing and returning the output is the postprocessing stage
 '''
+
+# funtions input and print 
+# print can be used to display info on screen
+# input can be used to display some info and prompt the user to enter a string 
+print("Quadratic Equation f(x)=ax**2+bx+c")
+
+## ask user to input coefficient values
+a=input("Enter value for a: ")
+b=input("Enter value for b: ")
+c=input("Enter value for c: ")
+
+## display user equation
+print("Equation is: (%s)x**2(%s)x+(%s)"%(a,b,c))
